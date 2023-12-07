8c443339cdb309ded833cbdc25da8990d0c95bf2
hassan2282 <taghavey.hassan@gmail.com>
2023-12-07 10:55:22 +0330

    comments and pages tables refactored

--- database/migrations/2023_12_04_204822_create_pages_table.php
+++ database/migrations/2023_12_04_204822_create_pages_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->nullable();
             $table->tinyInteger('status')->default(0);
             $table->string('tags');
             $table->softDeletes();
