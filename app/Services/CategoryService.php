6cab98674006ee3d5c99e3b5e6e795dbaa611017
hassan2282 <taghavey.hassan@gmail.com>
2023-12-24 22:02:28 +0330

    category service cleaned

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -4,8 +4,6 @@ namespace App\Services;
 
 use App\Models\Admin\Category;
 use App\Repositories\BaseRepository;
-use Illuminate\Support\Collection;
-use function Sodium\add;
 
 class CategoryService extends BaseRepository
 {
@@ -14,29 +12,9 @@ class CategoryService extends BaseRepository
         parent::__construct($category);
     }
 
-    public function all(): Collection
-    {
-        return $this->model->all('id', 'name');
-    }
-
     public function status($category)
     {
         $category->status = $category->status == 1 ? 0 : 1;
         $category->save();
-    }
-
-    public function create(array $attributes): Category
-    {
-        return $this->model->create($attributes);
-    }
-
-    public function update(array $attributes, $id): bool
-    {
-        return $this->model->update($attributes, $id);
-    }
-
-    public function delete($id)
-    {
-        return $this->find($id)->delete();
     }
 }
