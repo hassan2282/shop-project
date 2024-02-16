9a9158d1063f7f486661dba084e9b3bac7396673
hassan2282 <taghavey.hassan@gmail.com>
2024-02-16 14:23:59 +0330

    user model test added

---
+++ tests/Feature/Models/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function testInsertData(): void
+    {
+        $user = User::factory()->create();
+        $user['password'] = '12345';
+        $this->assertDatabaseHas('users', $user->toArray());
+        $this->assertDatabaseCount('users',1);
+    }
+}
